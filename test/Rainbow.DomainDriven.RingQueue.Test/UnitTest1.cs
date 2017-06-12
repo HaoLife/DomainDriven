2993d72fc1ecaf6e53a45ec2c5a6c4b9f6a94b7a
liuhao <w438418754@live.cn>
2017-06-12 23:19:37 +0800

    添加单元测试框架

---
+++ test/Rainbow.DomainDriven.RingQueue.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Rainbow.DomainDriven.RingQueue.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
