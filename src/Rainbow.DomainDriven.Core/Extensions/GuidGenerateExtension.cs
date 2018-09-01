1f6db97155105c1b9d5405162dafbd068f114e7c
liuhao <w438418754@live.cn>
2018-09-01 14:09:09 +0800

    guid自增运算

--- src/Rainbow.DomainDriven.Core/Extensions/GuidGenerateExtension.cs
+++ src/Rainbow.DomainDriven.Core/Extensions/GuidGenerateExtension.cs
@@ -23,38 +23,45 @@ namespace Rainbow.DomainDriven.Core.Extensions
             return new Guid(result.ToString());
         }
 
+        private static Random rm = new Random();
+        private readonly static int _size = 5;
         private static Byte[] _hostNameBytes;
 
         private static Byte[] HostNameBytes
         {
             get
             {
-                if (_hostNameBytes == null || _hostNameBytes.Length != 7)
+                if (_hostNameBytes == null || _hostNameBytes.Length != _size)
                 {
-
+                    //生成一个随机跳过位 32- size -1
+                    var skip = rm.Next(24);
                     var temp = System.Net.Dns.GetHostName().ToGuid().ToByteArray();
-                    _hostNameBytes = temp.Take(7).ToArray();
+                    _hostNameBytes = temp.Skip(skip).Take(_size).ToArray();
                 }
                 return _hostNameBytes;
             }
         }
-        private static long _seq = 0;
-        private static long[] _seqs = new long[2];
+        private static long[] _seqs = new long[3];
 
 
         public static Guid ToSequenceGuid(this DateTime time)
         {
-            var m = BitConverter.GetBytes(time.Ticks);
-            //位运算 0 or 1 ,这里取ticks的末尾第二位作为随机数
-            var index = (time.Ticks>>1) & 1;
-            //为运算 1111 1111 获取0-255
-            var e = (byte)((_seqs[index]++) & 0xFF);
+            var timeBytes = BitConverter.GetBytes(time.Ticks);
+            //为运算 1111 1111 获取0-65535
+            int rx = rm.Next(65535);
             byte[] bytes = new byte[16];
-            HostNameBytes.CopyTo(bytes, 0);
-            m.CopyTo(bytes, 7);
-            bytes[15] = e;
-            //重制另一个为0
-            _seqs[(index + 1) & 1] = 0;
+
+            //"03 02 01 00 - 05 04 - 07 06 - 08 09 - 0a 0b 0c 0d 0e 0f"
+            var timeReveseBytes = timeBytes.Skip(4).Take(4)
+                .Union(timeBytes.Skip(2).Take(2))
+                .Union(timeBytes.Take(2))
+                .ToArray();
+
+            timeReveseBytes.CopyTo(bytes, 0);
+            HostNameBytes.CopyTo(bytes, 8);
+            bytes[13] = (byte)((_seqs[0]++) & 0xFF);
+            bytes[14] = (byte)(rx & 0xFF);
+            bytes[15] = (byte)((rx >> 8) & 0xFF);
             return new Guid(bytes);
         }
     }
