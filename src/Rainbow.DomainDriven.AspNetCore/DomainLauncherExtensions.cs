f85a904295c433cf2f96d876062951a459903415
hao.liu <hao.liu@weimob.com>
2018-11-26 17:52:49 +0800

    添加aspcore的use写法

---
+++ src/Rainbow.DomainDriven.AspNetCore/DomainLauncherExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Rainbow.DomainDriven.Framework;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DomainLauncherExtensions
+    {
+        public static IApplicationBuilder UseDomain(this IApplicationBuilder builder)
+        {
+            var launcher = builder.ApplicationServices.GetRequiredService<IDomainLauncher>();
+            launcher.Start();
+            return builder;
+        }
+    }
+}
